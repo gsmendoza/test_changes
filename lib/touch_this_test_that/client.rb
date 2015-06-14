1b9da48c2b51b4089935265022d3d29cc7b09da6
George Mendoza <gsmendoza@gmail.com>
2015-06-14 11:41:37 +0800

    Refactor client.

--- lib/touch_this_test_that/client.rb
+++ lib/touch_this_test_that/client.rb
@@ -1,19 +1,43 @@
 module TouchThisTestThat
   class Client
     def call
-      commit = 'HEAD'
-
-      match_by_pattern = {
-        %r{^lib/(.+)\.rb} => 'spec/\1_spec.rb'
-      }
-
-      paths_changed_since_commit =
-        `git diff --name-only --diff-filter=AMR #{commit}`.split("\n")
-
       puts "\paths_changed_since_commit:"
       puts paths_changed_since_commit.inspect
 
-      matches = paths_changed_since_commit.map do |path|
+      puts "\nmatches:"
+      puts matches.inspect
+
+      puts "\nexisting_matches:"
+      puts existing_matches.inspect
+
+      system("rspec #{existing_matches.join(' ')}") if existing_matches.any?
+    end
+
+    private
+
+    def commit
+      'HEAD'
+    end
+
+    def match_by_pattern
+      @match_by_pattern ||= {
+        %r{^lib/(.+)\.rb} => 'spec/\1_spec.rb'
+      }
+    end
+
+    def paths_changed_since_commit
+      @paths_changed_since_commit ||=
+        `git diff --name-only --diff-filter=AMR #{commit}`.split("\n")
+    end
+
+    def existing_matches
+      @existing_matches ||= matches.select { |match| File.exist?(match) }
+    end
+
+    def matches
+      return @matches if @matches
+
+      @matches = paths_changed_since_commit.map do |path|
         matching_patterns = match_by_pattern.select do |pattern, _|
           path =~ pattern
         end
@@ -23,19 +47,7 @@ module TouchThisTestThat
         end
       end
 
-      matches = matches.reduce(:+).compact.uniq
-
-      puts "\nmatches:"
-      puts matches.inspect
-
-      existing_matches = matches.select { |match| File.exist?(match) }
-
-      puts "\nexisting_matches:"
-      puts existing_matches.inspect
-
-      if existing_matches.any?
-        system("rspec #{existing_matches.join(' ')}")
-      end
+      @matches = @matches.reduce(:+).compact.uniq
     end
   end
 end
